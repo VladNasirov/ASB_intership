d7aae07a689cc6e0f53cf1b52fdda3601dbca493
VladNasirov <93814899+VladNasirov@users.noreply.github.com>
2021-11-12 16:04:39 +0300

    Add files via upload

--- work.m
+++ work.m
@@ -1,9 +1,9 @@
-file='floatres.txt';
+file='C:\Users\makas\source\repos\Work\Work\floatres.txt';
 [Fisxodnoe]=importdata(file);
 subplot(2,1, 1)
 plot(Fisxodnoe,'o-');
-
+ylabel('A*10');
 subplot(2,1, 2)
-file='doubleres.txt';
+file='C:\Users\makas\source\repos\Work\Work\doubleres.txt';
 [Disxodnoe]=importdata(file);
 plot(Disxodnoe,'o-');
